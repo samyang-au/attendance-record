f823966ad18e2c6701c1b7e145bdb55e53fbac89
Sam Yang <samyang.au@gmail.com>
2020-11-27 21:07:01 +1000

    * access denied component

---
+++ client/src/authentication/access-denied.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { T } from 'translations/translate'
+
+import './access-denied.scss'
+
+export const AccessDenied = () => (
+    <div className="access-denied">
+        <T k="access-denied" />
+    </div>
+)
